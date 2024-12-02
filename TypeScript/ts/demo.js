3d60c224a19671de614e966902fb06dcfff636f7
bejesh-git <bejeshsriram2808@gmail.com>
2024-12-02 16:52:01 +0530

    'notes'

---
+++ TypeScript/ts/demo.js
@@ -0,0 +1,12 @@
+console.log("Demo TypeScript File");
+// String Data
+var fname;
+fname = "Bejesh";
+var lname = "Sriram";
+console.log("FIrst Name ".concat(fname, " Last Name:").concat(lname));
+//Number
+var num1 = 100, num2 = 0.5;
+console.log(num1, num2);
+//boolean
+var cond1 = true, cond2 = false;
+console.log(cond1, cond2);
---
+++ TypeScript/ts/demo.ts
@@ -0,0 +1,19 @@
+console.log("Demo TypeScript File");
+
+
+// String Data
+let fname:string;
+fname="Bejesh";
+let lname:string = "Sriram";
+
+console.log(`FIrst Name ${fname} Last Name:${lname}`);
+
+
+//Number
+let num1:number = 100, num2:number=0.5;
+console.log(num1,num2);
+
+
+//boolean
+let cond1:boolean=true, cond2:boolean=false;
+console.log(cond1, cond2);
