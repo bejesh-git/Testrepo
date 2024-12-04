e879be9f7f96a5512f642e3704de6b370c39f426
homecomputer <bejeshsriram2808@gmail.com>
2024-12-04 20:50:28 +0530

    notes

--- react/reactproject1/src/App.js
+++ react/reactproject1/src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import ClassCom from './component/ClassCom';
 import ConditionalRendering from './component/ConditionalRendering';
